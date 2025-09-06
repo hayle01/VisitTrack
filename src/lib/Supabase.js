760af000d93070ef34d63131499bfeb7e392dcb8
hayle01 <mabdirahim832@gmail.com>
2025-09-06 18:36:45 +0300

    Resolved import issue

--- src/lib/Supabase.js
+++ src/lib/Supabase.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
 const supabaseANONKEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseURL, supabaseANONKEY, {
+export const supabase = createClient(supabaseURL, supabaseANONKEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true 
